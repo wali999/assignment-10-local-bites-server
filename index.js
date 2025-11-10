3b4e0a358b743ae745a2447ddce030ec597fb295
wali999 <waliullah.neo@gmail.com>
2025-11-11 00:41:29 +0600

    get method for fetch allReviews added

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e8hxcyy.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('localbites-db')
+        const reviewCollection = db.collection('reviews')
+
+        app.get('/allReviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running!')
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
