987ae5d444c20af6d4235c931aea4bfef5d14b63
wali999 <waliullah.neo@gmail.com>
2025-11-12 16:10:08 +0600

    Delete method added for MyReviews

--- index.js
+++ index.js
@@ -68,6 +68,16 @@ async function run() {
 
 
 
+        //Delete option for my reviews
+        app.delete('/myReviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
